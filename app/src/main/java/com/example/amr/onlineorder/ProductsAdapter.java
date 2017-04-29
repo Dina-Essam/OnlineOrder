890b23582311de26fbee2e2140d732c688d1696f
AmrAbdelhameed <amrabdelhameedfcis123@gmail.com>
2017-04-30 00:04:06 +0200

    wla 3mlt 7aga :"D

--- app/src/main/java/com/example/amr/onlineorder/ProductsAdapter.java
+++ app/src/main/java/com/example/amr/onlineorder/ProductsAdapter.java
@@ -1,9 +1,7 @@
 package com.example.amr.onlineorder;
 
 import android.content.Context;
-import android.content.Intent;
 import android.graphics.Color;
-import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,10 +13,6 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 
 import java.util.List;
-
-/**
- * Created by Belal on 2/23/2017.
- */
 
 public class ProductsAdapter extends RecyclerView.Adapter<ProductsAdapter.ViewHolder> {
 
@@ -51,44 +45,6 @@ public class ProductsAdapter extends RecyclerView.Adapter<ProductsAdapter.ViewHo
 
         Glide.with(context).load(upload.getUrl()).into(holder.imageView);
 
-        holder.imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Bundle dataBundle = new Bundle();
-                dataBundle.putString("id_pro", upload.getId());
-                dataBundle.putString("name_pro", upload.getName());
-                dataBundle.putString("price_pro", upload.getPrice());
-                dataBundle.putString("image_pro", upload.getUrl());
-                Intent i = new Intent(context, SelectHowtoDoProduct.class);
-                i.putExtras(dataBundle);
-                context.startActivity(i);
-            }
-        });
-
-//        holder.imageView.setOnLongClickListener(new View.OnLongClickListener() {
-//
-//            @Override
-//            public boolean onLongClick(View v) {
-//
-//
-//                return true;
-//            }
-//        });
-//
-//        holder.textViewName.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Toast.makeText(context, upload.getName(), Toast.LENGTH_SHORT).show();
-//            }
-//        });
-//
-//        holder.overflow.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//
-//                Toast.makeText(context, upload.getPrice(), Toast.LENGTH_SHORT).show();
-//            }
-//        });
     }
 
     @Override
--- app/src/main/java/com/example/amr/onlineorder/User.java
+++ app/src/main/java/com/example/amr/onlineorder/User.java
@@ -2,9 +2,6 @@ package com.example.amr.onlineorder;
 
 import com.google.firebase.database.IgnoreExtraProperties;
 
-/**
- * Created by Amr on 24/04/2017.
- */
 @IgnoreExtraProperties
 public class User extends Person {
 
