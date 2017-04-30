2d531329c3783f5676cff21cf9ae5b4d9a2130d2
Dina Essam <dina.essam.fcis@gmail.com>
2017-04-30 22:49:06 +0200

    Add Order Class

---
+++ app/src/main/java/com/example/amr/onlineorder/Order.java
@@ -0,0 +1,69 @@
+package com.example.amr.onlineorder;
+
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Dina on 4/27/2017.
+ */
+
+public class Order implements Serializable {
+
+    String userID;
+    String id;
+    String brand_id;
+    Double totalPrice;
+    String state;
+    ArrayList<Product> items;
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getBrand_id() {
+        return brand_id;
+    }
+
+    public void setBrand_id(String brand_id) {
+        this.brand_id = brand_id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public ArrayList<Product> getItems() {
+        return items;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setItems(ArrayList<Product> items) {
+        this.items = items;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setTotalPrice(Double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+}
