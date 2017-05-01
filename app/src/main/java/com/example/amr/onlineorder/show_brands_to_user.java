c10756524ad70abd5f52c55144aa338f0b8956b1
Dina Essam <dina.essam.fcis@gmail.com>
2017-05-01 13:35:11 +0200

    Order List to user

--- app/src/main/java/com/example/amr/onlineorder/show_brands_to_user.java
+++ app/src/main/java/com/example/amr/onlineorder/show_brands_to_user.java
@@ -93,12 +93,16 @@ public class show_brands_to_user extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
 
 
-        if (item.getItemId() == R.id.user_orders) {
+        if (item.getItemId() == R.id.user_orders)
+        {
 
             /**
-             * hn2lo l layout h3red feha l products bs hdelha l user id bundle
+             * hn2lo l layout h3red feha l Orders bs hdelha l user id bundle
              */
-        } else if (item.getItemId() == R.id.logout_user) {
+        Intent order =new Intent(show_brands_to_user.this,show_orders_to_user.class);
+            startActivity(order);
+        }
+        else if (item.getItemId() == R.id.logout_user) {
 
             firebaseAuth.signOut();
             Intent logout = new Intent(show_brands_to_user.this, StartActivity.class);
