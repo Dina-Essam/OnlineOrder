361a487f58b98ac513007390617b015392420aeb
Dina Essam <dina.essam.fcis@gmail.com>
2017-05-04 20:16:36 +0200

    Filter and change state

--- app/src/main/java/com/example/amr/onlineorder/OnlineOrder.java
+++ app/src/main/java/com/example/amr/onlineorder/OnlineOrder.java
@@ -9,11 +9,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -21,6 +25,9 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.amr.onlineorder.R.id.spinnerstatus;
 
 public class OnlineOrder extends AppCompatActivity {
     ListView viewAllOrders;
@@ -28,6 +35,12 @@ public class OnlineOrder extends AppCompatActivity {
     ArrayList<Product> products;
     DatabaseReference mDataRef;
     ArrayList<Order> orderlist;
+    private DatabaseReference mF;
+    private DatabaseReference mFirebaseDatabase;
+    private FirebaseDatabase mFirebaseInstance;
+    DatabaseReference mData;
+    String username="";
+    Spinner search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +50,12 @@ public class OnlineOrder extends AppCompatActivity {
         orderlist = new ArrayList<>();
         viewAllOrders = (ListView) findViewById(R.id.listViewOrders);
         FirebaseDatabase database = FirebaseDatabase.getInstance();
+        mFirebaseInstance = FirebaseDatabase.getInstance();
+        final Spinner state=(Spinner)findViewById(R.id.spinnerstatus);
 
+        mFirebaseDatabase = mFirebaseInstance.getReference("Order");
+
+        search=(Spinner)findViewById(R.id.spinnerfilter);
 
         /**
          * fill Orders
@@ -82,6 +100,54 @@ public class OnlineOrder extends AppCompatActivity {
         });
 
 
+
+
+
+        /**
+         * search
+         */
+
+        try {
+
+            search.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    ArrayList<Order> filterOrders = new ArrayList<>();
+
+                    viewAllOrders.setAdapter(null);
+
+                    for (int y = 0; y < orderlist.size(); y++)
+                    {
+
+                        if (search.getSelectedItem().toString().equals(orderlist.get(y).state)) {
+                            filterOrders.add(orderlist.get(y));
+                        }
+                    }
+
+                    CustomAdapter myAdapter = new CustomAdapter(filterOrders);
+                    viewAllOrders.setAdapter(myAdapter);
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+
+                    CustomAdapter myAdapter = new CustomAdapter(orderlist);
+                    viewAllOrders.setAdapter(myAdapter);
+                }
+            });
+
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(OnlineOrder.this, e.getMessage(), Toast.LENGTH_LONG).show();
+
+        }
+
+
+
+
+
+
     }
 
 
@@ -113,19 +179,116 @@ public class OnlineOrder extends AppCompatActivity {
         @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
         @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
         @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
+        public View getView(final int position, View convertView, ViewGroup parent) {
 
             LayoutInflater linflater = getLayoutInflater();
+
             View view1 = linflater.inflate(R.layout.row_order_of_admin, null);
+            final Spinner state=(Spinner)view1.findViewById(R.id.spinnerstatus);
+
+            if(orderlist.get(position).getState().equals("Pending"))
+            {
+                List<String> list=new ArrayList<String>();
+                list.add("Pending");
+                list.add("InProgress");
+                list.add("Delivered");
+
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(OnlineOrder.this,android.R.layout.simple_spinner_item,list);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                state.setAdapter(adapter);
+
+            }
+            else if(orderlist.get(position).getState().equals("InProgress"))
+            {
+                List<String> list=new ArrayList<String>();
+                list.add("InProgress");
+                list.add("Pending");
+                list.add("Delivered");
+
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(OnlineOrder.this,android.R.layout.simple_spinner_item,list);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                state.setAdapter(adapter);
+            }
+            else if(orderlist.get(position).getState().equals("Delivered"))
+            {
+                List<String> list=new ArrayList<String>();
+                list.add("Delivered");
+                list.add("Pending");
+                list.add("InProgress");
+
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(OnlineOrder.this,android.R.layout.simple_spinner_item,list);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                state.setAdapter(adapter);
+            }
+
 
             TextView ordername = (TextView) view1.findViewById(R.id.productsname);
             for (Product s : orderlist.get(position).items) {
                 ordername.append(s.getName() + " \n");
             }
-            // ordername.setText("Order " + (position + 1));
+
+
+            /**
+             * User Name
+             */
+            progressDialog.show();
+            mData = FirebaseDatabase.getInstance().getReference();
+            mData.child("users").addValueEventListener(new ValueEventListener() {
+
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    Iterable<DataSnapshot> children = dataSnapshot.getChildren();
+
+                    progressDialog.dismiss();
+
+                    for (DataSnapshot child : children) {
+
+                        String uid = child.child("id").getValue().toString();
+                        String name = child.child("name").getValue().toString();
+                        if (orderArrayList.get(position).userID.equals(uid)) {
+
+                            username = name;
+
+                        }
+                    }
+
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+
+
+
+
+            state.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view,int sposition, long id) {
+                    String s = ((String) parent.getItemAtPosition(sposition));
+
+
+
+                    mFirebaseInstance = FirebaseDatabase.getInstance();
+                    mF = mFirebaseInstance.getReference("Order");
+                    mF.child(orderArrayList.get(position).getId()).child("state").setValue(state.getSelectedItem().toString());
+
+
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+                    // TODO Auto-generated method stub
+                }
+            });
+
+
+
 
             TextView userIDD = (TextView) view1.findViewById(R.id.customer);
-            userIDD.setText(orderArrayList.get(position).userID);
+            userIDD.setText("Cust: "+username);
 
             TextView orderprice = (TextView) view1.findViewById(R.id.totalprice);
             orderprice.setText("Total Price: " + orderArrayList.get(position).totalPrice.toString() + " LE");
